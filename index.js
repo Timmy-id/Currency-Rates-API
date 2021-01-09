00d00995856fd96f5d34941b530de0ea4051ce94
Oluwatimilehin <oluwatimilehin.id@gmail.com>
2021-01-10 00:38:28 +0100

    Added the project files

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+var PORT = process.env.port || 3000 
+
+const app = express();
+
+app.get("/api/rates", async (req, res) => {
+    const base = req.query.base;
+    const currency = req.query.symbols;
+    
+    const url = `https://api.exchangeratesapi.io/latest?base=${base}&symbols=${currency}`;
+    const options = {
+        "method": "GET",
+    };
+    const response = await fetch(url, options)
+        .then(res => res.json())
+        .catch(e => {
+            res.status(404).json({ "message": "Invalid data", error: e, });
+        });
+
+    res.status(200).json({
+        results: {
+            base: response.base,
+            date: response.date,
+            rates: response.rates
+        }
+    });
+});
+
+app.listen(PORT, (error) => {
+    if(error) throw error
+    console.log('App listening on PORT', PORT);
+});
